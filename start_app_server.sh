ca78e4bee842b705e4cce49c6f0003629a480f5c
sushi2k <sven.schleier@team.telstra.com>
2018-02-23 13:48:30 +0800

    removed creds for db

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://devseccon:devseccon2018@ds247058.mlab.com:47058/devseccon
+#export MONGODB_CREDENTIALS=mongodb://:@ds247058.mlab.com:47058/devseccon
 
 # finally, run the app in dev mode...
 npm run dev
